f1d0cb09ab8b2e1ee5ff8e9938fb933928dcf6db
tomomiKIDA <kida.biansha@gmail.com>
2020-08-11 03:52:47 +0000

    0811success

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -5,8 +5,19 @@
 		<meta http-equiv="X-UA-Compatible" content="IE-edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>MyNews</title>
+		@extends('layouts.admin')
 	</head>
 	<body>
 		<h1>Myニュース作成画面</h1>
+		@section('title', 'ニュースの新規作成')
+		@section('content')
+		    <div class="container">
+		        <div class="row">
+		            <div class="col-md-8 mx-auto">
+		                <h2>ニュース新規作成</h2>
+		            </div>
+		        </div>
+		    </div>
+		@endsection
 	</body>
 </html>
