489590e5d1c7b90b1499f0b9b726b9bae2126975
tomomiKIDA <kida.biansha@gmail.com>
2020-08-11 04:57:54 +0000

    081104success

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -4,9 +4,19 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE-edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>MyCreate</title>
+		<title>MyProfile</title>
 	</head>
 	<body>
-		<h1>Myクリエイト作成画面</h1>
+		<h1>Myプロフィール</h1>
+		@section('title')
+		@section('content')
+		    <div class="container">
+		        <div class="row">
+		            <div class="col-md-8 mx-auto">
+		                <h2>My プロフィール</h2>
+		            </div>
+		        </div>
+		    </div>
+		@endsection
 	</body>
 </html>
