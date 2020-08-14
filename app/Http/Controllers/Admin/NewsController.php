7f0e7f299fb74aa1abb6297c6de777873bc4adc8
tomomiKIDA <kida.biansha@gmail.com>
2020-08-14 09:11:14 +0000

    success

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,10 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\News;
 class NewsController extends Controller
 {
   public function add()
